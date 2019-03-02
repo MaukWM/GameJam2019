0b4bb7d721c96f0758d9bdc1122ba5cdbe59a055
H.C. Jansen <chris9898@live.nl>
2019-03-02 17:44:19 +0100

    ben nog bezig

--- controllers/menu_controller.py
+++ controllers/menu_controller.py
@@ -1,24 +1,37 @@
 import pygame
 import sys
 import time
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH, FRAME_RATE
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, FRAME_RATE, DEV_MODE
 
 
 class MenuController(object):
 
+    meme_mode = False
+
     def __init__(self):
         # setup stuff
         pygame.init()
-        window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+        self.window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
         pygame.font.init()
-        font = pygame.font.SysFont("Arial", 20)
-
+        self.font = pygame.font.SysFont("Arial", 20)
+        self.title_font = pygame.font.SysFont("Arial", 60)
 
     # Do all necessary setup
     def setup(self):
-        pass
+        if not DEV_MODE:
+            self.window.fill((133, 133, 133))
 
+            # draws the title
+            title_1_surface = self.title_font.render("METEOR DISASTER", False, (255, 255, 255))
+            title_2_surface = self.title_font.render("MINER DELUXE!", False, (255, 255, 255))
+            self.window.blit(title_1_surface, (330, 10))
+            self.window.blit(title_2_surface, (380, 70))
 
+            # meme mode checkbox
+            meme_mode_checkbox = pygame.image.load('assets/graphics/menu/unchecked.png')
+            meme_mode_text = self.title_font.render("meme mode", False, (255, 255, 255))
+            self.window.blit(meme_mode_checkbox, (420, 180))
+            self.window.blit(meme_mode_text, (550, 200))
 
     # handle a pressed key event in the context of the game root
     def handle_key_press(self, event_key):
@@ -27,6 +40,15 @@ class MenuController(object):
             pygame.quit()
             sys.exit()
 
+    def toggle_meme_mode(self):
+
+
+    def handle_mouse(self):
+        mouse_pos = pygame.mouse.get_pos()
+        x, y = int(mouse_pos[0]), int(mouse_pos[1])
+        if 420 <= x <= 420+109:
+            if 180 <= y <= 180+109:
+                self.toggle_meme_mode()
 
     # Handle all pygame events
     def handle_events(self):
@@ -36,6 +58,8 @@ class MenuController(object):
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
                     self.handle_key_press(event.key)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.handle_mouse()
 
 
     # Do all updates to the game state in this function
