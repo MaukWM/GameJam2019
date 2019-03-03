852562bc0ac703f74e2dee43a04d2c003f65390d
JD <jodopro@hotmail.com>
2019-03-03 12:46:28 +0100

    remove a print

--- models/tiles/dirt_tile.py
+++ models/tiles/dirt_tile.py
@@ -27,8 +27,6 @@ class Dirt(Tile):
                 surface.blit(DIRT_SPRITE, (x, y))
             else:
                 surface.blit(DIRT_BEAM_SPRITE, (x, y))
-        if not self.solid:
-            print(self.stability)
 
 
     def is_grass(self):
